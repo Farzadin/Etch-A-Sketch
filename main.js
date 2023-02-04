0fcb4a73a3c9658c265a8016948119199fc4ee43
thisiscyiran <farzad.dianat@gmail.com>
2023-02-04 23:52:00 +0330

    Set up a “hover” effect to grid divs
    Set up a “hover” effect so that the grid divs change color
    when your mouse passes over them using js eventListener

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
+// create element div then add css class `container` to it
 const container = document.createElement("div");
 container.classList.add("container");
+document.body.appendChild(container);
 
-const body = document.body.appendChild(container);
-
+// create 256 divs then add class `grid` to them
 for (let i = 1; i <= 256; i++) {
   const div = document.createElement("div");
+  div.classList.add(`grid`);
   container.appendChild(div);
 }
+
+// add "mouseover" event to all divs with the class of 'grid'
+// then change their background color when being targeted with
+// that event
+const grids = document.querySelectorAll(".grid");
+grids.forEach((grid) => {
+  grid.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = "blue";
+  });
+});
