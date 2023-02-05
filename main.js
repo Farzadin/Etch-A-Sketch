9a3da29a23078262aa721061b16826da0882c346
thisiscyiran <farzad.dianat@gmail.com>
2023-02-05 22:42:35 +0330

    Add a button that will send the user a popup
    Add a button to the top of the screen that will send the user a popup
    asking for the number of squares per side for the new grid.
    Once entered, the existing grid should be removed

--- main.js
+++ main.js
@@ -1,21 +1,37 @@
-// create element div then add css class `container` to it
+// container
 const container = document.createElement("div");
 container.classList.add("container");
 document.body.appendChild(container);
 
-// create 256 divs then add class `grid` to them
+// btn
+const btn = document.createElement("button");
+btn.classList.add("btn");
+document.body.insertBefore(btn, container);
+btn.textContent = "Change Grid-Size";
+
+// 256 squares
 for (let i = 1; i <= 256; i++) {
   const div = document.createElement("div");
   div.classList.add(`grid`);
   container.appendChild(div);
 }
 
-// add "mouseover" event to all divs with the class of 'grid'
-// then change their background color when being targeted with
-// that event
+// squares - mouseover event
 const grids = document.querySelectorAll(".grid");
 grids.forEach((grid) => {
   grid.addEventListener("mouseover", (event) => {
     event.target.style.backgroundColor = "blue";
   });
+});
+
+// btn - click event
+btn.addEventListener("click", (event) => {
+  const squares = prompt(
+    "Enter number of squares per side for the new grid: ",
+    0
+  );
+
+  while (container.hasChildNodes) {
+    container.removeChild(container.firstChild);
+  }
 });
