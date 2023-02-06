88f63fdb533b6e2d7b2c2a9fb0936b2429a3ba0d
thisiscyiran <farzad.dianat@gmail.com>
2023-02-06 22:23:56 +0330

    fix(cgs-button): :ambulance: Prompt value no longer can be negative or greater than 100
    new conditions has been added to pervent users from using values greater than 100 and less than 0 (negative) also the value no longer can be a string
    
    prompt no longer accept int values greater than 100 or less than 0

--- main.js
+++ main.js
@@ -9,6 +9,11 @@ btn.classList.add("btn");
 document.body.insertBefore(btn, container);
 btn.textContent = "Change Grid-Size";
 
+// message
+const msg = document.createElement("div");
+document.body.insertBefore(msg, container);
+msg.style.color = "red";
+
 // Add Hovering effect to grid's squares so that grid divs change color
 function addMouseOverEvent() {
   const grids = document.querySelectorAll(".grid");
@@ -53,7 +58,15 @@ btn.addEventListener("click", (event) => {
     0
   );
 
-  const totalSquares = square * square;
-  removeGrid();
-  makeGrid(totalSquares, square, square);
+  if (square < 0) {
+    msg.textContent = "sorry amount of squares per side cant be negative";
+  } else if (square > 100) {
+    msg.textContent = "amount of squares per sides cant be greater than 100";
+  } else if (isNaN(square)) {
+    msg.textContent = "please use only numbers";
+  } else {
+    const totalSquares = square * square;
+    removeGrid();
+    makeGrid(totalSquares, square, square);
+  }
 });
